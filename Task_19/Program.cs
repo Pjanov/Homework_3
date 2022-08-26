25a740adf7d37182b5953f3f3b6132ad4bb9de37
Андрей <pjanov84@mail.ru>
2022-08-26 12:51:26 +0300

    I wrote a program for Task_19

--- Task_19/Program.cs
+++ Task_19/Program.cs
@@ -4,5 +4,24 @@
 
 14212 -> нет
 12821 -> да
-23432 -> да
-*/
+23432 -> да     */
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+int temporary = num; // Объявляем временную переменную со значением исходного числа
+int invertedNum = 0; // Число перевернутое
+ 
+while (temporary != 0)
+{
+    invertedNum = invertedNum * 10 + temporary % 10; // Переворачиваем число по цифрам
+    temporary /= 10; // Отбрасываем последнюю цифру
+}
+ 
+if (num == invertedNum)
+{
+    Console.WriteLine("Число ЯВЛЯЕТСЯ палиндромом");
+}
+else
+{
+    Console.WriteLine("Число НЕЯВЛЯЕТСЯ палиндромом");
+}
